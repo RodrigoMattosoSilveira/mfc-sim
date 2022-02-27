4a61f24e487f7dae30d52a8d7146e5b084eadf2e
Rodrigo Silveira <rodrigo.silveira@ohi.com>
2022-02-26 18:48:17 -0800

    Added a parameter file

---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export type ParametersT = {
+    TIME_TO_WALK_TO_INVENTORY_AREA_MIN: number,
+    TIME_TO_WALK_TO_INVENTORY_AREA_MAX: number,
+    "TIME_TO_PICK_ITEM_MIN": number,
+    "TIME_TO_PICK_ITEM_MAX": number
+}
