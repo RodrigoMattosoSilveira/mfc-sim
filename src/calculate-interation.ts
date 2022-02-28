171e5246bfc6a749678704956b380a6f0311e67c
Rodrigo Silveira <rodrigo.silveira@ohi.com>
2022-02-28 11:19:34 -0800

    0002-account-for-breaks Added an OrderId capability

--- src/calculate-interation.ts
+++ src/calculate-interation.ts
@@ -2,15 +2,17 @@ import {ParametersT} from "./types";
 
 export const calculateIterationF = (fsm: any, parameters: ParametersT): void => {
     fsm.log();
-    // console.log(`fsm.state: I'm at the ` + fsm.state);
-    fsm.pick();
-    // console.log(`fsm.state: I'm at the ` + fsm.state);
-    fsm.pack();
-    // console.log(`fsm.state: I'm at the ` + fsm.state);
-    fsm.label();
-    // console.log(`fsm.state: I'm at the ` + fsm.state);
-    fsm.deliver();
-    // console.log(`fsm.state: I'm at the ` + fsm.state);
-    fsm.ready();
-    // console.log(`fsm.state: I'm at the ` + fsm.state);
-}
+    for (let i: number = 0; i < 10; i++) {
+        // console.log(`fsm.state: I'm at the ` + fsm.state);
+        fsm.pick();
+        // console.log(`fsm.state: I'm at the ` + fsm.state);
+        fsm.pack();
+        // console.log(`fsm.state: I'm at the ` + fsm.state);
+        fsm.label();
+        // console.log(`fsm.state: I'm at the ` + fsm.state);
+        fsm.deliver();
+        // console.log(`fsm.state: I'm at the ` + fsm.state);
+        fsm.ready();
+        // console.log(`fsm.state: I'm at the ` + fsm.state);
+    }
+ }
--- src/on-pick.ts
+++ src/on-pick.ts
@@ -1,8 +1,12 @@
 import {ParametersT} from "./types";
+import { nanoid } from 'nanoid';
 
 const random = require("random");
 
 export const pickF = (parameters: ParametersT): void => {
+    // Get an order number
+    const orderId = nanoid();
+
     // walk to the inventory area
     let time_to_walk_to_inventory_area = random.int(parameters.TIME_TO_WALK_TO_INVENTORY_AREA_MIN,parameters.TIME_TO_WALK_TO_INVENTORY_AREA_MAX);
     console.log('I walked to the inventory area in: ' + time_to_walk_to_inventory_area + ' seconds');
